166763e3d571e336f480ec718213b09ea2302e12
Tamim Mahmud <internationalinf24@gmail.com>
2022-08-03 00:37:11 +0600

    Array in js

---
+++ app.js
@@ -0,0 +1,12 @@
+const number = [24, 15, 84, 95];
+const number1 = new Array(15, 61, 75, 15);
+const color = ["Red", "Yellow", "Blue"];
+const mixed = [24, new Date(), "Hello", 45, "World", {a: 1, b: 2}];
+console.log(mixed);
+
+let value;
+
+value = number.length;
+value = Array.isArray(number);
+console.log(number);
+console.log(value);
